af8e8a0bd82be9fc59ee5bd490b4612408c4b738
Piotr Błądek <piotr.bladek@gft.com>
2018-05-16 12:47:51 +0200

    Init

---
+++ src/main/java/com/gft/dbbenchmark/DbBenchmarkApplication.java
@@ -0,0 +1,12 @@
+package com.gft.dbbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbBenchmarkApplication.class, args);
+	}
+}
